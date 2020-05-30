0edba54f40e1a8405d0936365d82896a61511006
admin <admin@admins-Mac.local>
2020-05-29 19:43:06 -0700

    added brew nad cask installs

--- homebrew.sh
+++ homebrew.sh
@@ -1,9 +1,48 @@
 #!/bin/sh
-##  Needs to be run as administrator
+
+## Note: this needs to be run as administrator
 
 if ! [ -x "$(command -v brew)" ]; then
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
-echo "hello"
+echo "== brew installed, checking for updates..."
+brew update
 
-exit 1
+brew list --versions bash || brew install bash
+
+PACKAGES=(
+	maven
+	the_silver_searcher
+	wget
+	openjdk
+	pyenv	
+)
+echo "== Installing Packages.."
+
+for exe in ${PACKAGES[@]}
+do
+	echo "== Checking $exe ==========="	
+	brew list --versions $exe || brew install $exe
+done
+
+CASKS=(
+	beyond-compare
+	nvalt
+	java
+	oracle-jdk
+	cheatsheet
+	iterm2
+)
+
+for c in ${CASKS[@]}
+do
+	echo "== Checking $c ==========="	
+	brew cask list --versions $c || brew cask install $c
+done
+
+
+
+echo "Cleaning up... "
+brew cleanup
+exit 0 
+
